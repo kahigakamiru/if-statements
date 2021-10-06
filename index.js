be0eb41d7e7a96b72a07cfa63af247accce70588
kahiga <kahigakamiru@gmail.com>
2021-10-06 08:54:21 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+function getGrade(score) {
+    let grade;
+
+    if (score <= 5) {
+        return console.log('F');
+    } else if (score <= 10) {
+        return console.log('E');
+    } else if (score <= 15) {
+        return console.log('D');
+    } else if (score <= 20) {
+        return console.log('C');
+    } else if (score <= 25) {
+        return console.log('B');
+    } else {
+        return console.log('A');
+    }
+
+    return grade;
+}
+const getInputAndDisplay = () => {
+    console.log("Please enter the score");
+    process.stdin.on("data", (data) => {
+    const score = data;
+    
+    
+    
+    getGrade(score);
+    process.exit();
+    });
+    };
+
+getInputAndDisplay();
